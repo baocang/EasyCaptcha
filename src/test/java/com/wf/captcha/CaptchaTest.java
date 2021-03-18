5219ee98d930f5ff19a5a9e5f3bc3360409a9ed0
Baocang Nie <baocang.nie@icloud.com>
2021-03-19 00:25:59 +0800

    Enable test

--- src/test/java/com/wf/captcha/CaptchaTest.java
+++ src/test/java/com/wf/captcha/CaptchaTest.java
@@ -1,9 +1,9 @@
 package com.wf.captcha;
 
-import org.junit.Test;
-
 import java.io.File;
 import java.io.FileOutputStream;
+
+import org.junit.Test;
 
 /**
  * 测试类
@@ -11,61 +11,74 @@ import java.io.FileOutputStream;
  */
 public class CaptchaTest {
 
+    private String prepOutputDir() {
+        String directory = ".captchas";
+        File folderFile = new File(directory);
+
+        if (folderFile.exists()) {
+            folderFile.delete();
+        }
+
+        folderFile.mkdirs();
+
+        return folderFile.getAbsolutePath() + File.separator;
+    }
+
     @Test
     public void test() throws Exception {
-        /*for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < 10; i++) {
             SpecCaptcha specCaptcha = new SpecCaptcha();
             specCaptcha.setLen(4);
             specCaptcha.setFont(i, 32f);
             System.out.println(specCaptcha.text());
-            specCaptcha.out(new FileOutputStream(new File("C:/Java/aa" + i + ".png")));
-        }*/
+            specCaptcha.out(new FileOutputStream(new File(prepOutputDir() + "spec_" + i + ".png")));
+        }
     }
 
     @Test
     public void testGIf() throws Exception {
-        /*for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < 10; i++) {
             GifCaptcha gifCaptcha = new GifCaptcha();
             gifCaptcha.setLen(5);
             gifCaptcha.setFont(i, 32f);
             System.out.println(gifCaptcha.text());
-            gifCaptcha.out(new FileOutputStream(new File("C:/Java/aa" + i + ".gif")));
-        }*/
+            gifCaptcha.out(new FileOutputStream(new File(prepOutputDir() + "gif_" + i + ".gif")));
+        }
     }
 
     @Test
     public void testHan() throws Exception {
-        /*for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < 10; i++) {
             ChineseCaptcha chineseCaptcha = new ChineseCaptcha();
             System.out.println(chineseCaptcha.text());
-            chineseCaptcha.out(new FileOutputStream(new File("C:/Java/aa" + i + ".png")));
-        }*/
+            String tempPath = System.getProperty("java.io.tmpdir") + File.separator;
+            chineseCaptcha.out(new FileOutputStream(new File(prepOutputDir() + "han_" + i + ".png")));
+        }
     }
 
     @Test
     public void testGifHan() throws Exception {
-        /*for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < 10; i++) {
             ChineseGifCaptcha chineseGifCaptcha = new ChineseGifCaptcha();
             System.out.println(chineseGifCaptcha.text());
-            chineseGifCaptcha.out(new FileOutputStream(new File("C:/Java/aa" + i + ".gif")));
-        }*/
+            chineseGifCaptcha.out(new FileOutputStream(new File(prepOutputDir() + "gif_han_" + i + ".gif")));
+        }
     }
 
     @Test
     public void testArit() throws Exception {
-        /*for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < 10; i++) {
             ArithmeticCaptcha specCaptcha = new ArithmeticCaptcha();
             specCaptcha.setLen(3);
             specCaptcha.setFont(i, 28f);
-            System.out.println(specCaptcha.getArithmeticString() + " " + specCaptcha.text());
-            specCaptcha.out(new FileOutputStream(new File("C:/Java/aa" + i + ".png")));
-        }*/
+            specCaptcha.out(new FileOutputStream(new File(prepOutputDir() + "arit_" + i + ".png")));
+        }
     }
 
     @Test
     public void testBase64() throws Exception {
-        /*GifCaptcha specCaptcha = new GifCaptcha();
-        System.out.println(specCaptcha.toBase64(""));*/
+        GifCaptcha specCaptcha = new GifCaptcha();
+        System.out.println(specCaptcha.toBase64(""));
     }
 
 }
